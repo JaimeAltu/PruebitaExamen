46bbbfcec73f7b75722388ea61e967bcf81bda28
jaime <jaime@LAPTOT-JAIME02>
2022-03-01 13:37:38 +0100

    PrimerCommit

---
+++ PruebaExamen/src/Pruebita.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author jaime
+ * @since 01/03/2022
+ * @version 1.0
+ */
+public class Pruebita {
+	int numero=22;
+	String nombre= "jaime";
+	String apellido = "altuzarra";
+	/**
+	 * 
+	 * @param numero
+	 * @param nombre
+	 * @param apellido
+	 */
+	public Pruebita(int numero, String nombre, String apellido) {
+	}
+	
+}
